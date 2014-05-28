eee940f9c77f765d2cfab9d15a1d06d502fa7805
Henrique <hrickmachado@gmail.com>
2014-05-28 16:49:59 -0300

    NHibernate Parte 2
    Ajustes no ppt com informações da classe de teste com o exemplo.

---
+++ Crowdlearning.NHibernate/Demo3/ValidatableTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crowdlearning.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NHibernate;
+using NHibernate.Classic;
+
+namespace Crowdlearning.Demo3
+{
+    [TestClass]
+    public class ValidatableTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ValidationFailure))]
+        public void ValidandoCriacaoPais()
+        {
+            try
+            {
+                using (ISession session = Context.SessionFactory.OpenSession())
+                {
+                    Pais pais = new Pais();
+                    pais.Nome = "A";
+                    session.Save(pais);
+                    session.Flush();
+                }
+            }
+            catch (ValidationFailure validationFailure)
+            {
+                Assert.AreEqual(validationFailure.Message, "Nome não pode ser melhor que 2.");
+                throw;
+            }
+        }
+    }
+}
